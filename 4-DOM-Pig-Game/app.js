d702af9925725952f770a58c215b5daa8dc7e0e0
jac.massa0908@gmail.com <jac.massa0908@gmail.com>
2018-05-21 14:06:20 -0500

    refactor action after game won

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -11,26 +11,8 @@ GAME RULES:
 
 var scores, roundScore;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
-
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>'+dice+'</em>';
-
-var  el = document.querySelector('#score-0').textContent;
-
-document.querySelector('.dice').style.display ='none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-
-// function btn(){
-//     //TODO
-// }
 
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
@@ -80,4 +62,31 @@ function nextPlayer(){
     document.querySelector('.player-1-panel').classList.toggle('active');
 
     document.querySelector('.dice').style.display ='none';
+}
+
+document.querySelector('.btn-new').addEventListener('click',init);
+
+function init(){
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector('.dice').style.display ='none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent ='Player 1';
+    document.getElementById('name-1').textContent ='Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+    document.querySelector('.player-0-panel').classList.add('active');
+
+
 }
