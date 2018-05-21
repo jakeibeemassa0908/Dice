8323fef6c0d49cfa8320bc9c8aa2cebb29676682
jac.massa0908@gmail.com <jac.massa0908@gmail.com>
2018-05-21 13:51:49 -0500

    implement dice winning algorithm

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -9,19 +9,75 @@ GAME RULES:
 
 */
 
-var scores, roundScore, dice;
+var scores, roundScore;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random()*6)+1;
-console.log(dice);
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>'+dice+'</em>';
 
 var  el = document.querySelector('#score-0').textContent;
-console.log(el);
 
 document.querySelector('.dice').style.display ='none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+
+// function btn(){
+//     //TODO
+// }
+
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    //1. Random Number
+    var dice = Math.floor(Math.random()*6)+1;
+
+    //2. Display Result
+    var diceImage=document.querySelector('.dice');
+    diceImage.style.display='block';
+    diceImage.src = 'dice-'+dice+'.png';
+
+
+    //3.update the round score if the rolled number ws not a 1
+
+    if(dice>1){
+        roundScore += dice;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }else{
+        nextPlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    scores[activePlayer] += roundScore;
+
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+    
+    if(scores[activePlayer]>=20){
+        document.getElementById('name-'+activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+    }else{
+        nextPlayer();
+    }   
+});
+
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1: activePlayer =0;
+    roundScore =0;
+
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display ='none';
+}
